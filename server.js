e7b9b1c720f60216cfc434762207bfb412160307
finproject-web <rickyjones544@gmail.com>
2026-04-29 19:45:09 -0700

    Recreate complete legal pages with full content

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const server = http.createServer((req, res) => {
   
   let filePath = '.' + req.url;
   if (filePath === './') {
-    filePath = './standalone.html';
+    filePath = './index.html';
   }
 
   const extname = String(path.extname(filePath)).toLowerCase();
